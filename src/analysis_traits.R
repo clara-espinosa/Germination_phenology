3e946e476323bad03abf52edb14f52cb99b31eee
Clara Espinosa del Alba <espinosaclara@uniovi.es>
2022-11-23 17:11:14 +0100

    t50 and heatsum calculated from model

--- src/analysis_traits.R
+++ src/analysis_traits.R
@@ -18,10 +18,11 @@ summary(temp)
 read.csv("data/clean data.csv", sep = ";") %>%
   mutate(date = strptime(as.character(date), "%d/%m/%Y"))%>%
   mutate(time = as.numeric(as.Date(date)) - min(as.numeric(as.Date(date)))) %>%
-  group_by(species, incubator, code, petridish) %>%
+  group_by(species, code, incubator, petridish) %>%
   filter(date == max(date)) %>%
-  select(species, incubator, code, petridish, viable) %>%
-  group_by(species, incubator) %>%
+  select(species, code, incubator, petridish, viable) %>%
+  #group_by(species, incubator) %>%
+  arrange (species, code, incubator, petridish)%>%
   summarise(viable = sum(viable)) -> viables
 
 ggplot (viables, aes(viable, incubator, fill = incubator)) +
@@ -32,10 +33,11 @@ ggplot (viables, aes(viable, incubator, fill = incubator)) +
 read.csv("data/clean data.csv", sep = ";") %>%
   mutate(date = strptime(as.character(date), "%d/%m/%Y"))%>%
   mutate(time = as.numeric(as.Date(date)) - min(as.numeric(as.Date(date)))) %>%
-  group_by(species, incubator) %>%
-  summarise(germinated = sum(germinated)) %>%
+  group_by(species, code, incubator, petridish) %>%
+  summarise(seeds_germ = sum(germinated)) %>% # 
   merge(viables) %>%
-  mutate(germination = (germinated/viable) *100) -> finalgerm 
+  mutate(germPER = (seeds_germ/viable) *100, # 
+         germPER = round (germPER, digit =2)) -> finalgerm 
 
 ggplot (finalgerm, aes(germination, incubator, fill= incubator))+
   geom_boxplot ()+
@@ -44,67 +46,23 @@ ggplot (finalgerm, aes(germination, incubator, fill= incubator))+
 # tidyverse modification to have time to reach 50% germination according to germ checks  ####
 read.csv("data/clean data.csv", sep = ";") %>%
   mutate(date = strptime(as.character(date), "%d/%m/%Y"))%>%
-  group_by (species) %>%
+  group_by (species, code, incubator, petridish) %>%
   mutate (datesow= first (date)) %>% # a different column for sowing date
-  mutate(time = as.numeric(as.Date(date)) - min(as.numeric(as.Date(date)))) %>% # time count from sowing of each sp
-  ungroup () %>%
-  group_by (species, incubator, time)%>% 
-  summarise(germinated = sum(germinated)) %>%
-  mutate(germination = cumsum(germinated)) %>%
+  mutate(days = difftime(date, min(date), units = "days")) %>% # time count from sowing of each petridish
+  arrange(species, code, incubator, petridish, days) %>% 
+  group_by (species, code, incubator, petridish, days) %>% 
+  summarise(seeds_germ = sum(germinated)) %>%
+  mutate(cum_seeds_germ = cumsum(seeds_germ)) %>%
   merge (viables) %>%
-  mutate(germination = (germination/viable) *100) %>%
-  ungroup() %>%
-  mutate (t50 = ifelse(germination > 50, time, 0)) %>% # CAMBIAR 0 POR TIME MAX
-  group_by (species, incubator) %>%
+  mutate(germPER = (cum_seeds_germ/viable) *100, # 
+         germPER = round (germPER, digit =2)) %>%
+  mutate (t50 = ifelse(germPER >= 50, days, 0)) %>% # CAMBIAR 0 POR TIME MAX
+  group_by (species, code, incubator, petridish) %>%
   mutate (t50check = first(t50[t50>0])) %>%
   summarise(t50check = min (t50check)) %>%
   ungroup()-> time50 # values discrete bc count t50 according to germination check days
 
 ggplot (time50, aes(t50check, incubator, fill= incubator)) +
-  geom_boxplot () +
-  theme_bw()
-
-# sowing date + t50 date or final date check ####
-read.csv("data/clean data.csv", sep = ";") %>%
-  mutate(date = strptime(as.character(date), "%d/%m/%Y"))%>%
-  group_by (species, incubator) %>%
-  mutate (datesow= first (date)) %>% 
-  mutate (datelast= last(date)) %>%
-  select(species, incubator, viable, germinated, datesow, datelast) %>%
-  merge (time50) %>%
-  group_by (species, incubator, datesow, datelast)%>% 
-  summarise(t50check = min (t50check)) %>%
-  mutate (date50 = as.Date(datesow) + t50check) %>%
-  mutate (date50 = replace_na (date50, as.character(datelast))) -> time50dates
-
-## traits Fellfield-Snowbed dataframe version 1 ####
-traits_FS <- full_join(finalgerm, time50dates, by= c("species", "incubator")) 
-
-### Heat:sum function####
-heatsum <-function (traits_FS) {
-  traits_FS %>%
-    pull (incubator)%>%
-    unique() -> inc
-  traits_FS %>%
-    pull(datesow) -> date1 #convierte columna en vector
-  traits_FS%>%
-    pull(date50) -> date2
-  temp %>% 
-    mutate (date = as.Date(date)) %>%
-    filter(incubator == inc) %>%
-    filter(date >= date1 & date <= date2) %>%
-    summarise (HS =sum(Tmean)) 
-}
-
-traits_FS%>%
-  group_by (species, incubator) %>%
-  do (heatsum(.)) %>% # punto significa que el objeto al que aplicar la funcion heat sum es el de la linea de arriba
-  data.frame -> heat_sum
-
-## traits Fellfield-Snowbed dataframe version 2 ####
-traits_FS <- full_join(traits_FS, heat_sum, by= c("species", "incubator")) 
-
-ggplot (traits_df, aes(HS, incubator, fill=HS)) +
   geom_boxplot () +
   theme_bw()
 
@@ -124,7 +82,7 @@ read.csv("data/clean data.csv", sep = ";") %>%
   mutate(cs = cumsum(germinated), # cumulative sum of germinated seeds 
          fg = max(cs) / viable, # max germinated (final germination) divided by viable seeds (proportion)
          g = cs / viable, # proportion of germination at each date
-         Timeframe = ifelse(g > 0.5, "Upper", "Lower")) %>% # # New column that divides dataset in germination < 0.5 or > 0.5
+         Timeframe = ifelse(g >= 0.5, "Upper", "Lower")) %>% # # New column that divides dataset in germination < 0.5 or > 0.5
   group_by(species, code, incubator, petridish, Timeframe) %>%
   mutate(t50times = ifelse(Timeframe == "Lower", max(days), min(days)), # keep only the number of days just before and after reaching t50
          t50g = ifelse(days == t50times, g, NA)) %>% # copy of germination proportion the days just before and after t50
@@ -134,13 +92,62 @@ read.csv("data/clean data.csv", sep = ";") %>%
   group_by(species, code, incubator, petridish, petricode) %>%
   filter(fg >= .50) %>% # filter to keep only the species that reach more than 0.5 in max final germination
   filter(length(petricode) > 1) %>% # not sure why this comand about lenght of petricode variable, to fix some strange error?
-  #group_by(species, code, incubator, petridish, petricode, fg) %>%
-  group_by (species, incubator) %>%
+  group_by(species, code, incubator, petridish) %>%
   do(f1(.)) %>%
   rename(#FG = fg, 
          t50lm = `(0.5 - as.numeric(mf1$coefficients[1]))/as.numeric(mf1$coefficients[2])`) ->t50model
 
-t50comparison <- full_join(time50, t50model, by = c ("species", "incubator"))
+t50comparison <- full_join(time50, t50model, by = c("species", "code", "incubator", "petridish")) 
+t50_trait <- t50comparison %>%
+  mutate (t50lm1 = t50lm) %>%
+  mutate_at(vars(t50lm1), ~ as.integer(round(.x))) %>% # round decimals to integer numbers
+  rename (t50lm_days = t50lm1) 
+str(t50model)
+
+# sowing date + t50 date or final date check ####
+read.csv("data/clean data.csv", sep = ";") %>%
+  mutate(date = strptime(as.character(date), "%d/%m/%Y"))%>%
+  group_by (species, code, incubator, petridish) %>%
+  mutate (datesow= first (date)) %>% 
+  mutate (datelast= last (date)) %>% 
+  select(species, code, incubator, petridish, viable, germinated, datesow, datelast) %>%
+  merge (t50_trait) %>%
+  group_by (species, code, incubator, petridish, datesow, datelast, t50lm_days)%>% 
+  summarise(t50check = min (t50check)) %>% 
+  mutate (date50 = as.Date(datesow) + t50lm_days) %>% 
+  mutate (date50 = replace_na (date50, as.character(datelast))) -> t50_dates
+
+
+## traits Fellfield-Snowbed dataframe version 1 ####
+traits_FS <- full_join(t50_dates, finalgerm,  by= c("species", "code", "incubator", "petridish")) 
+
+### Heat:sum function####
+heatsum <-function (traits_FS) {
+  traits_FS %>%
+    pull (incubator)%>%
+    unique() -> inc
+  traits_FS %>%
+    pull(datesow) -> date1 #convierte columna en vector
+  traits_FS%>%
+    pull(date50) -> date2
+  temp %>% 
+    mutate (date = as.Date(date)) %>%
+    filter(incubator == inc) %>%
+    filter(date >= date1 & date <= date2) %>%
+    summarise (HS =sum(Tmean)) 
+}
+
+traits_FS%>%
+  group_by (species, code, incubator, petridish) %>%
+  do (heatsum(.)) %>% # punto significa que el objeto al que aplicar la funcion heat sum es el de la linea de arriba
+  data.frame -> heat_sum
+
+## traits Fellfield-Snowbed dataframe version 2 ####
+traits_FS <- full_join(traits_FS, heat_sum, by= c("species", "code", "incubator", "petridish")) 
+
+ggplot (traits_df, aes(HS, incubator, fill=HS)) +
+  geom_boxplot () +
+  theme_bw()
 
 # undersnow germination (nÂº of seeds) ####
 snow <- read.csv("data/clean data.csv", sep = ";") %>%
