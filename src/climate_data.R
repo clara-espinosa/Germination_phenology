524c09e90610e68470e378d4b55d2b6d0bb1331e
Clara Espinosa del Alba <espinosaclara@uniovi.es>
2023-02-08 17:12:11 +0100

    climate script more organized

--- src/climate_data.R
+++ src/climate_data.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-### Temporal survey PICOS 10 years of data ###
+### Annual indices PICOS 10 years of data ##
 read.csv("data/temperatures-picos.csv", sep = ";") %>%
   mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% #specify format of TIME variable
   mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
@@ -37,9 +37,11 @@ read.csv("data/temperatures-picos.csv", sep = ";") %>%
   #mutate(Survey = "Temporal") 
   select(Site, bio1, bio2, bio7, Snw, FDD, GDD)%>% 
   group_by(Site)%>%
-  summarise_all(mean, na.rm = TRUE) %>%
-  write.csv("data/annual-indices.csv")
+  summarise_all(mean, na.rm = TRUE) -> annual_picos
 
+#write.csv("data/annual-indices.csv")
+
+### Annual indices Omaña 1year of data ##
 read.csv("data/temperatures-villabandin.csv", sep = ";") %>% # exclude Cañada site bc data only goes for 2 months
   mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% #specify format of TIME variable
   mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
@@ -68,6 +70,7 @@ read.csv("data/temperatures-villabandin.csv", sep = ";") %>% # exclude Cañada s
             FDD = abs(sum(FDD)), # FDD per year
             GDD = sum(GDD)) -> annual_villa
 
+## PICOS WEEKLY values mean, max, min, snow, fdd, gdd
 read.csv("data/temperatures-picos.csv", sep = ";") %>%
   mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% #specify format of TIME variable
   mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
@@ -86,22 +89,27 @@ read.csv("data/temperatures-picos.csv", sep = ";") %>%
   mutate(FreezeThaw = ifelse(X > 0.5 & N < -0.5, 1, 0)) %>% # Day with freeze-thaw cycles
   mutate(FDD = ifelse(T < 0, T, 0)) %>% # Freezing degrees per day
   mutate(GDD = ifelse(T >= 1, T, 0)) %>% # Growing degrees day per month https://link.springer.com/article/10.1007/s00035-021-00250-1
-  #TOMORROW CHECK https://www.statology.org/r-group-by-week/
-  group_by(Site, Month = lubridate::floor_date(Day, "month")) %>%
-  summarise(T = mean(T), X = mean(X), N = mean(N), # Daily mean, max, min
-            Snow = sum(Snow), # Snow days per month
-            FreezeThaw = sum(FreezeThaw), # Freeze-thaw days per month
-            FDD = sum(FDD), # FDD per month
-            GDD = sum(GDD)) %>% # GDD per month
-  group_by(Site, Year = lubridate::floor_date(Month, "year")) %>%
-  summarise(bio1 = mean(T), # Annual Mean Temperature
-            bio2 = mean(X - N), # Mean Diurnal Range (Mean of monthly (max temp - min temp))
-            bio7 = max(X) - min(N), # Temperature Annual Range (BIO5-BIO6)
-            Snw = sum(Snow),
-            FDD = abs(sum(FDD)), # FDD per year
-            GDD = sum(GDD)) %>%
-  mutate(Year = as.character(lubridate::year(Year))) %>% 
-  rename(Plot = Year) %>%
-  mutate(Survey = "Temporal") -> 
-  dfTime
-# temperatures in Omaña july 2021- july 2022
+  mutate (week = strftime(Day, format = "%V")) %>%
+  select(Site, T, X, N, Snow, FDD, GDD, week)%>%
+  group_by (Site, week) %>%
+  summarise_all(mean, na.rm = TRUE) -> weekly_picos 
+
+### Omaña weekly values 1year of data ##
+read.csv("data/temperatures-villabandin.csv", sep = ";") %>% # exclude Cañada site bc data only goes for 2 months
+  mutate(Time = strptime(as.character(Time), "%d/%m/%Y %H:%M"))%>% #specify format of TIME variable
+  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
+  mutate(Month = lubridate::month(Time)) %>% 
+  mutate(Day = lubridate::day(Time)) %>% 
+  mutate(Hour = lubridate::hour(Time)) %>% 
+  filter(! Month %in% 6) %>% # Remove missing June days (loggers buried later in rabinalto)
+  dplyr::select(-c(Month, Day, Hour)) %>%
+  group_by(Site, Day = lubridate::floor_date(Time, "day")) %>%
+  summarise(T = mean(Temperature), X = max(Temperature), N = min(Temperature), n = length(Time)) %>% # Daily mean, max, min
+  mutate(Snow = ifelse(X < 0.5 & N > -0.5, 1, 0)) %>% # Day is snow day or not
+  mutate(FreezeThaw = ifelse(X > 0.5 & N < -0.5, 1, 0)) %>% # Day with freeze-thaw cycles
+  mutate(FDD = ifelse(T < 0, T, 0)) %>% # Freezing degrees per day
+  mutate(GDD = ifelse(T >= 1, T, 0)) %>% # Growing degrees day per month https://link.springer.com/article/10.1007/s00035-021-00250-1
+  mutate (week = strftime(Day, format = "%V")) %>%
+  select(Site, T, X, N, Snow, FDD, GDD, week)%>%
+  group_by (Site, week) %>%
+  summarise_all(mean, na.rm = TRUE) -> weekly_villa
